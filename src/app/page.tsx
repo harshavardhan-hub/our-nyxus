ab511494c2104754cf1c2b10c9567d9e82c71582
harshavardhan-hub <yanakandlaharshavardhan@gmail.com>
2026-03-12 21:37:52 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import HeroSection from "@/components/HeroSection";
-import TechStackSection from "@/components/TechStackSection";
 import SelectedWorkSection from "@/components/SelectedWorkSection";
 import DifferenceSection from "@/components/DifferenceSection";
 import ProcessSection from "@/components/ProcessSection";
 import StatsSection from "@/components/StatsSection";
 import ServicesSection from "@/components/ServicesSection";
-import TeamSection from "@/components/TeamSection";
-import TestimonialsSection from "@/components/TestimonialsSection";
 import FaqSection from "@/components/FaqSection";
 import CallToAction from "@/components/CallToAction";
 
@@ -14,14 +11,11 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      <TechStackSection />
       <SelectedWorkSection />
       <DifferenceSection />
       <ProcessSection />
       <StatsSection />
       <ServicesSection />
-      <TeamSection />
-      <TestimonialsSection />
       <FaqSection />
       <CallToAction />
     </>
